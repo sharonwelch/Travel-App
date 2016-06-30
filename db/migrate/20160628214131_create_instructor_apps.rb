73ae27f069d95b2872ea8d491dd50979bcf23200
ellismiskell <ellis.burgoonmiskell@yale.edu>
2016-06-30 16:28:53 -0400

    change arguments in instructors and instructor_apps models

--- db/migrate/20160628214131_create_instructor_apps.rb
+++ db/migrate/20160628214131_create_instructor_apps.rb
@@ -1,15 +1,22 @@
 class CreateInstructorApps < ActiveRecord::Migration
   def change
     create_table :instructor_apps do |t|
-      t.integer :instructor_id
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.string :instagram_handle
+      t.string :blog_url
+      t.string :facebook_url
       t.string :other_social_media
+      t.string :picture
+      t.string :activities
       t.text :love_about_hometown
       t.text :who_are
       t.text :unique_thing
       t.text :activity_desc
       t.string :activity_title
       t.string :why_share
-      t.string :photos
       t.string :workflow_state
 
       t.timestamps null: false

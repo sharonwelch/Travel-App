f89875f0f726169b2d9ef16952b045136c2cc6eb
Sharon <sharon.welch@yale.edu>
2016-07-06 14:00:24 -0400

    made each fullpage element take up the whole screen

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,10 @@
 $(function() {
     $('#fullpage').fullpage({
       sectionsColor: ['#76DB9F', '#4BBFC3', 'whitesmoke'],
+<<<<<<< Updated upstream
+=======
+      // fitToSection: true,
+      // headerColor: ['#76DB9F', '#4BBFC3', 'whitesmoke']
+>>>>>>> Stashed changes
     });
 });

eaee547992c153286995a75133a2f9b2efe9a950
Sharon <sharon.welch@yale.edu>
2016-07-06 09:22:59 -0400

    edited the fonts on the homepage

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,8 @@
 $(function() {
     $('#fullpage').fullpage({
       sectionsColor: ['#76DB9F', '#4BBFC3', 'whitesmoke'],
+      // fitToSection: true,
+      // headerColor: ['#76DB9F', '#4BBFC3', 'whitesmoke']
+      vertical
     });
 });

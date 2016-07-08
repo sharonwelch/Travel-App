c258c3ddcb860e85383ec3fbbf89ceb37ee873a5
Zoe Henry <zoe.henry@yale.edu>
2016-07-08 17:46:37 -0400

    fixed centering issue on front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @name = "Quirk"
-    @greeting = "Connect.Learn.Create."
+    @greeting = "Connect. Learn. Create."
     @mission_statement = "Don't just see things, experience them. <br> Learn the local lifestyle from the passions of the people who live there.".html_safe
-    @browse_locals = "See the local character. Create your experience."
+    @browse_locals = "See the local character. <br> Create your own experience.".html_safe
   #  @apply =
  end
 

9c4c2f89505563c4dc02b070db7e6edfaf1a097d
Zoe Henry <zoe.henry@yale.edu>
2016-07-14 15:41:06 -0400

    scss changes

--- app/controllers/contact_info_controller.rb
+++ app/controllers/contact_info_controller.rb
@@ -6,7 +6,6 @@ class ContactInfoController < ApplicationController
     @names = svc.contact[0]
     @emails = svc.contact[1]
     @contacts = []
-    binding.pry
     0.upto(@names.length - 1) do |x|
       name = @names[x]
       email = @emails[x]

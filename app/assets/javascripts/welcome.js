85398d9d92fa591ac4857cf3e7b062f103549a60
ellismiskell <ellis.burgoonmiskell@yale.edu>
2016-07-18 12:28:28 -0400

    fixed devise and scheduler datetime

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -9,4 +9,12 @@ $(function() {
       // fixedElements: '#header',
     });
     hasLoaded = true;
+
+    $('#possible_start_1').datetimepicker();
+    $('#possible_end_1').datetimepicker();
+    $('#possible_start_2').datetimepicker();
+    $('#possible_end_2').datetimepicker();
+    $('#possible_start_3').datetimepicker();
+    $('#possible_end_1').datetimepicker();
+
 });

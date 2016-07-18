96dfaffdaa3ad3f7e6ed1ccc1a96fea1161f7722
Sharon <sharon.welch@yale.edu>
2016-07-18 18:40:09 -0400

    Fix fullpage stuff and git errors! - John

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,20 +1,13 @@
-var hasLoaded = false;
-
 $(function() {
-    if (hasLoaded) {
-      $.fn.fullpage.destroy('all');
-    }
-    $('#fullpage').fullpage({
-      // sectionsColor: ['#76DB9F', '#4BBFC3', 'whitesmoke'],
-      // fixedElements: '#header',
-    });
-    hasLoaded = true;
+  $('#fullpage').fullpage({
+    paddingTop: "80px",
+    verticalCentered: true
+  });
 
-    $('#possible_start_1').datetimepicker();
-    $('#possible_end_1').datetimepicker();
-    $('#possible_start_2').datetimepicker();
-    $('#possible_end_2').datetimepicker();
-    $('#possible_start_3').datetimepicker();
-    $('#possible_end_1').datetimepicker();
-
+  $('#possible_start_1').datetimepicker();
+  $('#possible_end_1').datetimepicker();
+  $('#possible_start_2').datetimepicker();
+  $('#possible_end_2').datetimepicker();
+  $('#possible_start_3').datetimepicker();
+  $('#possible_end_1').datetimepicker();
 });

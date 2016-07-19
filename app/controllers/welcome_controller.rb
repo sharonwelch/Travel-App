eb8ad9716164b33e9578a3826c9213f1433d2a38
Zoe Henry <zoe.henry@yale.edu>
2016-07-18 22:42:15 -0400

    i cleaned out all of unnecessary code whooooooooop

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,22 +4,6 @@ class WelcomeController < ApplicationController
     @greeting = "Connect. Learn. Create."
     @mission_statement = "Don't just see things, experience them. <br> Learn the local lifestyle from the passions of the people who live there.".html_safe
     @browse_locals = "See the local character. <br> Create your own experience.".html_safe
-  #  @apply
-
  end
 
-
- def instructor_profile
-
- end
-
- def stories
- end
-
- # def user_reviews
- #   @activities_to_review = Activity.not_reviewed
- # end
-
- def user_profile
- end
 end
